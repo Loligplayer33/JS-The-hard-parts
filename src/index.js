1fcb921a633eb1b82fcf52e8e0c0d8941914a059
David Haase <dhaase033@gmail.com>
2020-05-04 21:51:14 +0200

    added asynchronous and OOP part

--- src/index.js
+++ src/index.js
@@ -291,3 +291,257 @@ incrementCounterClone2();
 
 // if you look in the console, the values start a 1 again like so: 1 2 3 4 and don't add up
 // to the other counter like so: ... 5 6 7 8.
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+console.log(".........Asynchronous Code!!!");
+
+/*************************************************************/
+/**Javascript is a single threaded language. But what if a line takes really
+ * long to complete because it gets data from an api of has to do something else
+ * that takes a lot of time?
+ * 
+ * THERE NEEDS TO BE A WAY THAT SOME CODE CAN RUN IN THE BACKGROUND WHILE
+ * THE THREAD OF EXECUTION CONTINUES
+ * 
+ * => all of the 'slow' tasks like setting a timer, fetching data and so on
+      don't happen in Javascript, but rather in the web browser.
+      So Javascript's thread of execution can keep on running, while the 
+      browser takes care of all the slow taks. 
+ */
+
+function printHello() {
+  // commented this console.log() out, because it would always be run at the end, after
+  //all other global code in this file would have been executed, which is distracting.
+  // console.log("hello");
+}
+
+function blockFor1Sec() {
+  var arr = [];
+
+  for (let i = 0; i < 10000; i++) {
+    arr.push(i);
+  }
+}
+
+setTimeout(printHello, 0);
+blockFor1Sec();
+console.log("me first");
+
+/**all the code that takes some time to complete and is completed with the help of
+ * the browser and not just JS gets put on the call stack, only if ALL OTHER CODE
+ * HAS EXECUTED AND RETURNED. INCLUDING THE GLOBAL CODE.
+ * => it runs after everything else is done!!!
+ * The place where it waits, to get into the call stack is called the 'callback queue'
+ * and the method that checks if all other code has executed is called the 'event loop'
+ */
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+console.log(".........promises!!!");
+
+// MOCK example
+
+// 1. the function 'display' gets stored in global memory
+function display(data) {
+  // 7. the data gets logged to the console
+  console.log(data);
+}
+
+// 2. declare a constant 'futureData'
+/**
+ * 4. fetch return the promise object (normal object with two properties:
+ * {
+   value: ...... (stores the returned result from the request)
+   onFullfilled: []  (stores the code that shoul be run after the data
+                       has come back (.then())) (hidden propterty)
+  }
+* 4.1 it sets up an 'xml http request' (xhr / network request)
+* 4.2 sends the name like (twitter.com) and the path (/will/tweets/1) with it
+  so it can get the results
+* 4.3 puts the response data into the 'value' property of the promise object
+*/
+// 5. the response then gets stored into futureData
+const futureData = fetch("https://twitter.con/will/tweets/1");
+//6. the display function gets called after the value property gets filled
+futureData.then(display);
+// 3. 'me first gets logged to the console'
+console.log("me first");
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+console.log(".........Classes & Prototypes!!!");
+
+// If you have a big file, you want to make sure that you structure your data and functionality
+//together, close to each other so it is not hard to find the corresponding pieces in the codebase.
+// => Objects are a great way to achieve this:
+
+const user1 = {
+  name: "Will",
+  score: 3,
+  increment: function() {
+    user1.score++;
+  }
+};
+
+console.log(user1.score);
+user1.increment();
+console.log(user1.score);
+
+// the problem with this approach is, that all the functions, like 'increment' have to
+// be stored manuallly which makes the code less DRY and very time consuming to type.
+
+// => to solve this issue, we have function constructors / classes:
+
+// This function constructor creates a new Object based on the inputed arguments and
+// returns out the newly created object.
+function userCreator(name, score) {
+  // it gets linked to 'userFunctionStore' thorugh the 'hidden propery (__proto__)';
+  const newUser = Object.create(userFunctionStore);
+  newUser.name = name;
+  newUser.score = score;
+
+  return newUser;
+}
+// here are all the functions stored that do the loginc based on the instances of
+// the object returned by 'userCreator'
+const userFunctionStore = {
+  increment: function() {
+    // 'score' is still available due to closures which took effect since the object,
+    //this function gets called on has to be returned out of the function. And trough that
+    //process, it takes all the needed variables from local memory and keeps them in global memory as persistent data.
+    this.score++;
+    // the 'this' always points to the thing that the method the 'this' sits in is called from
+  },
+  login: function() {
+    console.log("Logged in");
+  }
+};
+
+// calling the userCreator function and creating a new Instance of the class based on the parameters.
+const user10 = userCreator("Will", 3);
+const user11 = userCreator("Tim", 3);
+
+// Since 'userCreator' is linked to 'userFunctionStore' through its __proto__ property,
+// it now has access to its functions.
+user10.increment();
+
+// => this is the STANDARD way of creating a function constructor
+
+// But there are much shorter forms, but this is what is happening under the hood.
+
+/****************************************************** */
+// THE 'new' KEYWORD
+
+/* 1. the new keyword creates the object needed automatically 
+*  2. it returns the object autoatically
+*  3. it will make the link through the __proto__ object automatically
+
+  => but how can we add new properties to the automatically created object, if we
+     don't have a name to do something like newUser.name = 'Todd' ? 
+      => we give it the name 'this' (this.name = 'Todd')
+
+  => and how does the 'new' keyword know, which Object it should link to in the
+     __proto__ property?
+     (
+      every function brings a second part with it. An Object, attached to every function;
+     )
+*/
+function iAmAlsoAnObject() {
+  return "hi";
+}
+
+iAmAlsoAnObject.prototype.score = 0;
+
+console.log(iAmAlsoAnObject.prototype);
+
+// => this is where the new keyword makes the __proto__ property reference to.
+// that's why this works:
+
+// 1. we declare a function (constructor)and store it in global memory (the function has an object with the 'prototype' property on it)
+function UserCreatorConstructor(name, score) {
+  this.name = name;
+  this.score = score;
+}
+
+// 2. we add a method to the userCreatorConstructor's prototype object
+UserCreatorConstructor.prototype.increment = function() {
+  this.score++;
+};
+
+// 3. we add another method to the userCreatorConstructor's prototype object
+UserCreatorConstructor.prototype.login = function() {
+  console.log("login");
+};
+
+// we declare a constant 'user12' and assign it the returned value of the code on the right side:
+/**
+ * 1. the 'new' keyword automatically creates a new empty object whichs __proto__ property
+ *    points to the Object part of the 'userCreatorConstructor' function and there
+ *    to the 'prototype' property
+ * 2. It automatically returns the now filled object through 'this.name' and 'this.score' (l. 498f.)
+ *    => the returned object has all the data it references to from its local memory in its closure (backpack)
+ * 3. The returned object gets assigned to the global constant 'user12'
+ */
+const user12 = new UserCreatorConstructor("Eva", 10);
+// now we can call the 'increment' method on 'user12' since it is linked to it via the __proto__ property
+user12.increment();
+// that's why the score is now incremented by one an
+console.log(user12);
+
+
+
+/************************************************** */
+// The 'class' syntactic sugar:
+console.log('class way:');
+
+
+class UserCreatorClass {
+  // this is where we define the isntance - individual data, the methods can then work with
+  // or they can just be safed and stored (same as the whole 'UserCreatorConstructor' function (l.487ff.))
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  // the methods are still stored in the protoype property of the function object through the __proto__ property (implicitly)
+  increment() { //no 'function' keyword needed to decalre methods in classes
+    this.score++
+  }
+
+  login() {
+    console.log('login');
+  }
+}
+
+const user113 = new UserCreatorClass('David', 15 )
+
+user113.increment();
+console.log(user113);
+
+// this is just another approach, but it works the exact same way
